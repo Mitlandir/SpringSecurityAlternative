2d56bd8dd07dd15904604668b20335a091fcb2b6
milan <mtimo101289@gmail.com>
2017-02-06 09:48:54 +0100

    rest seems to be working now

--- src/main/java/com/online_exchange/controller/RestController.java
+++ src/main/java/com/online_exchange/controller/RestController.java
@@ -2,9 +2,12 @@ package com.online_exchange.controller;
 
 import com.online_exchange.dao.TransactionDao;
 import com.online_exchange.dao.TransactionDaoImpl;
+import com.online_exchange.model.Client;
 import com.online_exchange.model.Completedtransaction;
 import com.online_exchange.model.Transactionoffer;
 import com.online_exchange.model.Transactionrequest;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
@@ -36,10 +39,38 @@ public class RestController {
         return transactionDao.sendCompletedtransaction(trans);
     }
 
-    @RequestMapping(value = "/test", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Transactionrequest test(@RequestBody Transactionrequest req) {
-        transactionDao.sendTransactionrequest(req);
-        return req;
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public List<Client> test() {
+        List<Client> list = new ArrayList<Client>();
+        Client client1 = new Client();
+        Client client2 = new Client();
+
+        client1.setEmail("yahoo");
+        client1.setPassword("konj");
+        client2.setEmail("hotmail");
+        client2.setPassword("mis");
+
+        list.add(client1);
+        list.add(client2);
+
+        return list;
+    }
+
+    @RequestMapping(value = "/test2", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Client> test2() {
+        List<Client> list = new ArrayList<Client>();
+        Client client1 = new Client();
+        Client client2 = new Client();
+
+        client1.setEmail("yahoo");
+        client1.setPassword("konj");
+        client2.setEmail("hotmail");
+        client2.setPassword("mis");
+
+        list.add(client1);
+        list.add(client2);
+
+        return list;
     }
 
 }

51e519c40bc5b33c3f59fdbf864f02bddc6192d0
milan <mtimo101289@gmail.com>
2017-02-13 14:41:05 +0100

    initiated client/exchanger controllers

---
+++ src/main/java/com/online_exchange/controller/ExchangerRestController.java
@@ -0,0 +1,11 @@
+
+package com.online_exchange.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/exchanger")
+public class ExchangerRestController {
+    
+}

a213728fda23b5dbb1a30f3dbc26d01f182d7b72
milan <mtimo101289@gmail.com>
2017-02-13 14:41:53 +0100

    initiated client/exchanger controllers

--- src/main/java/com/online_exchange/controller/ClientRestController.java
+++ src/main/java/com/online_exchange/controller/ClientRestController.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.online_exchange.controller;
 
-/**
- *
- * @author mtimotijevic
- */
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/client")
 public class ClientRestController {
     
 }
